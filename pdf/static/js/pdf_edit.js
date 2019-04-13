9627521e71ecfec8bd260aaaa75e6c4c663dc4cc
chen_wei <chen_wei0104@wuxiapptec.com>
2019-04-13 17:09:21 +0800

    添加后台取数逻辑

--- pdf/static/js/pdf_edit.js
+++ pdf/static/js/pdf_edit.js
@@ -1,5 +1,9 @@
 /* Javascript for pdfXBlock. */
 function pdfXBlockInitEdit(runtime, element) {
+    var dataUrl = runtime.handlerUrl(element, 'get_select_data');
+    var url_selector = $(element).find('#pdf_select_url');
+
+    var selectList =[{'label': 'initLable', 'url': 'init-url'}]
 
     $(element).find('.action-cancel').bind('click', function() {
         runtime.notify('cancel', {});
@@ -13,9 +17,9 @@ function pdfXBlockInitEdit(runtime, element) {
             'source_text': $('#pdf_edit_source_text').val(),
             'source_url': $('#pdf_edit_source_url').val()
         };
-        
+
         runtime.notify('save', {state: 'start'});
-        
+
         var handlerUrl = runtime.handlerUrl(element, 'save_pdf');
         $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
             if (response.result === 'success') {
@@ -27,4 +31,27 @@ function pdfXBlockInitEdit(runtime, element) {
             }
         });
     });
+
+    $.ajax({
+        type: "POST",
+        url: dataUrl,
+        data: JSON.stringify({requested: true}),
+        success: set_select_data
+    });
+    
+    function set_select_data(result) {
+        console.log('get select data from backend');
+        console.log(result);
+        selectList = result;
+        var selectOpts = '';
+        if(selectList.length >0 ){
+            for (let i = 0; i < selectList.length; i++) {
+                var obj = selectList[i]
+                var opt = '<option value =' + obj.url + '>' + obj.label + '</option>'
+                selectOpts += opt
+            }
+        }
+        console.log('add options,',selectOpts);
+        url_selector.html(selectOpts)
+    }
 }
